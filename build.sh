d1b3b8ccb2df3c3e8826d269e22e4b534e95d7e1
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-03-08 11:09:43 +0000

    added build and status scripts

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd
+cd workspace/cmdev
+
+cd cm-pom
+mvn clean install
+cd ..
+
+cd euclid
+mvn clean install
+cd ..
+
+cd svg
+mvn clean install
+cd ..
+
+cd html
+mvn clean install
+cd ..
+
+cd imageanalysis
+mvn clean install
+cd ..
+
+cd pdf2svg
+mvn clean install
+cd ..
+
+cd svg2xml
+mvn clean install
+cd ..
+
+cd cproject
+mvn clean install
+cd ..
+
+cd norma
+mvn clean install
+cd ..
+
+cd diagramanalyzer
+mvn clean install
+cd ..
+
+cd ami
+mvn clean install
+cd ..
+
+
---
+++ status.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+cd
+cd workspace/cmdev
+
+cd cm-pom
+echo "======= CM-POM ==="
+git pull
+git status
+git push
+cd ..
+
+cd euclid
+echo "======= EUCLID ========"
+git pull
+git status
+git push
+cd ..
+
+cd svg
+echo "======= SVG ========"
+git pull
+git status
+git push
+cd ..
+
+cd html
+echo "======= HTML ========"
+git pull
+git status
+git push
+cd ..
+
+cd imageanalysis
+echo "======= IMAGEANALYSIS ========"
+git pull
+git status
+git push
+cd ..
+
+cd pdf2svg
+echo "======= PDF2SVG ========"
+git pull
+git status
+git push
+cd ..
+
+cd svg2xml
+echo "======= SVG2XML ========"
+git pull
+git status
+git push
+cd ..
+
+cd cproject
+echo "======= CPROJECT ========"
+git pull
+git status
+git push
+cd ..
+
+cd norma
+echo "======= NORMA ========"
+git pull
+git status
+git push
+cd ..
+
+cd diagramanalyzer
+echo "======= DIAGRAMANALYZER ========"
+git pull
+git status
+git push
+cd ..
+
+cd ami
+echo "======= AMI ========"
+git pull
+git status
+git push
+cd ..
+
+
