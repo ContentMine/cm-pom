1d1ac4705af7c99f42c74c788acd4d12dcea4297
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-03-13 07:53:16 +0000

    tweaked build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 # cd to the parent directory of the 11 modules
 # this command should show at least the 11 directories 
 
+date
 
 if [ ! -d cm-pom ]; then
 	echo "Cannot find directory cm-pom; found: "
@@ -13,18 +14,19 @@ if [ ! -d cm-pom ]; then
 fi
 	
 set -e
+# modules. Can comment out/in but ONLY ONE space
 dirs=( \
-	cm-pom \
-    euclid \
-    svg \ 
-    html \
-    imageanalysis \
-    pdf2svg \
-    svg2xml \
-    cproject \
-    norma \
-#     diagramanalyzer \		
-#     ami \
+cm-pom \
+euclid \
+svg \
+html \
+imageanalysis \
+pdf2svg \
+svg2xml \
+cproject \
+norma \
+#diagramanalyzer \		
+#ami \
 )
 for i in "${dirs[@]}";
         do cd $i;
@@ -33,3 +35,4 @@ for i in "${dirs[@]}";
         cd ../;
 done;
 
+date
